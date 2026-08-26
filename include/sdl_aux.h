c30570e3fb90d7e5edac77acf2e07034cf8f4792
Raposo <rapogabriel1@gmail.com>
2026-08-26 20:47:12 -0300

    Inicio da implementação da janela com SDL3

---
+++ include/sdl_aux.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+SDL_HitTestResult SDLCALL regras_da_janela(SDL_Window *, const SDL_Point *, void *);
--- src/main.c
+++ src/main.c
@@ -1,11 +1,52 @@
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include <stdio.h>
+#include <SDL3/SDL.h>
+#include "sdl_aux.h"
 
-#include "arg_parse.h"
-
-int main(int argc, char* argv[]){
+int main(void){
     printf("Olá mundo!\n");
-    // Placeholder
-    [[maybe_unused]]
-    parseResul ind = arg_parse(argc, argv);
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Falha ao inicializar SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (!SDL_CreateWindowAndRenderer("Chip-8 Emulator", 800, 640, SDL_WINDOW_BORDERLESS, &window, &renderer)) {
+        printf("Erro ao criar janela/renderizador: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_FRect bar = { 0.0f, 0.0f, 800.0f, 40.0f };
+    bool rodando = true;
+    SDL_Event evento;
+
+    SDL_SetWindowHitTest(window, regras_da_janela, nullptr);
+
+    while (rodando) {
+    
+        while (SDL_PollEvent(&evento)) {
+            if(evento.type == SDL_EVENT_QUIT){
+                rodando = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 20, 20, 50, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+        SDL_RenderFillRect(renderer, &bar);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
