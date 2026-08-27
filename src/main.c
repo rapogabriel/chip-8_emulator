99171223693917bbee618a8e351242722fe092f6
Raposo <rapogabriel1@gmail.com>
2026-08-26 21:43:28 -0300

    Testes iniciais com icones personalizados na barra do programa

--- src/main.c
+++ src/main.c
@@ -23,6 +23,33 @@ int main(void){
         return 1;
     }
 
+    SDL_Surface* surface_icone = SDL_LoadBMP("../imgs/close_icon.bmp");
+    if (!surface_icone) {
+        printf("Erro ao carregar o ícone: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_SetSurfaceColorKey(surface_icone, true, SDL_MapRGB(SDL_GetPixelFormatDetails(surface_icone->format), NULL, 255, 255, 255));
+
+    SDL_Texture* textura_icone = SDL_CreateTextureFromSurface(renderer, surface_icone);
+    if (!textura_icone) {
+        printf("Erro ao criar textura do ícone: %s\n", SDL_GetError());
+        SDL_DestroySurface(surface_icone);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_DestroySurface(surface_icone);
+
+    SDL_FRect local_icone = { 
+        .x = 800.0f - 24.0f - 10.0f, // 800 - largura_icone - margem_direita
+        .y = (40.0f - 24.0f) / 2.0f, // (altura_barra - altura_icone) / 2 (para centralizar)
+        .w = 24.0f,
+        .h = 24.0f
+    };
     SDL_FRect bar = { 0.0f, 0.0f, 800.0f, 40.0f };
     bool rodando = true;
     SDL_Event evento;
@@ -41,6 +68,7 @@ int main(void){
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
         SDL_RenderFillRect(renderer, &bar);
+        SDL_RenderTexture(renderer, textura_icone, NULL, &local_icone);
         SDL_RenderPresent(renderer);
     }
 
